3b68d7285f96a9913fc38a268e9b489b2cce5d31
ajay <ajayhemanth90@gmail.com>
2026-08-30 17:47:10 +0530

    if-else statements

---
+++ ifelse.java
@@ -0,0 +1,10 @@
+public class ifelse{
+    public static void main(String[] a){
+        int age = 18;
+        if(age >= 18){
+            System.out.print("Major");
+        }else{
+            System.out.print("Minor");
+        }
+    }
+}
