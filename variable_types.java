78828ac8cb9776faaaff21bda5ad2939202ca85b
ajay <ajayhemanth90@gmail.com>
2026-09-04 12:49:19 +0530

    Variable declaration

---
+++ variable_types.java
@@ -0,0 +1,13 @@
+public class variable_types {
+    public static void main(String[] args) {
+        int num = 5;
+        byte num1 = 10;
+        short num2 = 20;
+        long num3 = 30;
+        double a = 4.68598596;
+        float b = 5.8f;
+        boolean c = true;
+        char ch = 'H';
+        String f = "Hello world";
+    }   
+}
