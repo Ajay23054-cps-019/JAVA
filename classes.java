c88a22381c4b5ce80e37817f7f51702e40c42804
ajay <ajayhemanth90@gmail.com>
2026-09-06 17:02:31 +0530

    class and object

---
+++ classes.java
@@ -0,0 +1,14 @@
+class cal{
+    public static int main(int n1,int n2){
+        return n1+n2;
+    }
+}
+
+
+
+public class classes{
+    public static void main(String[] args) {
+        cal obj = new cal();
+        System.out.println(obj.main(5,8));
+    }
+}
